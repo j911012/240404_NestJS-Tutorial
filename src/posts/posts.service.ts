c12525a4b3cf4f425191e98c6c84c41bf5eec31f
jun <juntetsu.1012@gmail.com>
2024-04-04 20:48:35 +0900

    postsモジュール作成

---
+++ src/posts/posts.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { PostType } from 'src/posts/post.interface';
+
+@Injectable()
+export class PostsService {
+  private readonly posts: PostType[] = [];
+
+  findAll(): PostType[] {
+    return this.posts;
+  }
+}
